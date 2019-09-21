69926b6ca16dd87e2f803b107a61c6b327178c86
dglav <glavich_drew@yahoo.com>
2019-09-21 15:33:32 +0900

    Foregoing node for PHP simplicity

--- index.js
+++
@@ -1,8 +0,0 @@
-var static = require("node-static");
-var file = new static.Server();
-
-require("http").createServer(function(req, res) {
-    req.addListener('end', function() {
-        file.serve(req, res);
-    }).resume();
-}).listen(process.env.PORT || 3000);
